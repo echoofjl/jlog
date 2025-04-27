3c4fad90fde7825a8912034e1be31f2cb214a5ba
JL <108511164+jessicali1625@users.noreply.github.com>
2025-04-27 16:04:12 +0800

    add some articles and change the style

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,10 +7,9 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		category: z.string(),
+		description: z.string().optional(),
 		tags: z.array(z.string()).optional(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
